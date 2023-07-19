545c33bb0581a0c8b0e2f9e1e403a81ededb0bb9
Zalman Zoteev <sz.zoteev@gmail.com>
2023-07-19 21:54:46 +0300

    Initial Commit

---
+++ semafor/semaforApp.swift
@@ -0,0 +1,17 @@
+//
+//  semaforApp.swift
+//  semafor
+//
+//  Created by Zalman Zoteev on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct semaforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
